0c39bd6652568e98447d87f2fede87fb3aa30f8d
Burak Avar <burakavar@gmail.com>
2023-07-16 17:50:23 +0300

    Write WriteLine Methods added.

--- Console Applications/Program.cs
+++ Console Applications/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Write WriteLine Methods
+
+//Question 1: Write "Hello World!" to the console using the Write method.
+static void Question1()
+{   
+    Console.Write("Hello, World!");
+}
+
+//Question 2: Write "Hello World!" to the console using the WriteLine method.
+void Question2()
+{
+    Console.WriteLine("Hello, World!");
+}
+
+//Question 3: Print "A=50B=25" to the console using the Write method.
+//Assign the value 50 to the variable A and the value 25 to the variable B.
+void Question3()
+{
+    int a,b;
+    a=50;
+    b=25;
+    Console.Write($"A={a}");
+    Console.Write($"B={b}");
+}
+
+
+//Question 4: Print "A=50B=25" to the console using the WriteLine method.
+//Assign the value 50 to the variable A and the value 25 to the variable B.
+void Question4()
+{
+    int A,B;
+    A=50;
+    B=25;
+    Console.WriteLine($"A={A}");
+    Console.WriteLine($"B={B}");
+}
+
