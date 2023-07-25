9145a50ac65282f1268d60336fb34ecbfe176350
Burak Avar <burakavar@gmail.com>
2023-07-25 22:53:12 +0300

    transfers and bitwise operators added.

--- Arithmetic Operators/Program.cs
+++ Arithmetic Operators/Program.cs
@@ -83,3 +83,61 @@ void Question5()
     Console.WriteLine("The amount of money in Dollar is {0:F2}", amount / dollar);
 }
 
+
+
+// Transfers Operators
+
+
+//Write a program that performs the sum , multiplication and mode operations of the
+//numbers entered by the user from the keyboard with abbreviated transfer operators.
+void Question6()
+{
+    int num1, num2;
+    Console.WriteLine("Enter the first number");
+    num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter the second number");
+    num2 = Convert.ToInt32(Console.ReadLine());
+    num1 += num2;
+    Console.WriteLine("The sum of the two numbers is {0}", num1);
+    num1 -= num2;
+    Console.WriteLine("The difference of the two numbers is {0}", num1);
+    num1 *= num2;
+    Console.WriteLine("The product of the two numbers is {0}", num1);
+    num1 /= num2;
+    Console.WriteLine("The quotient of the two numbers is {0}", num1);
+    num1 %= num2;
+    Console.WriteLine("The mode of the two numbers is {0}", num1);
+}
+
+
+
+//Bitwise Operators
+
+//Write a program that prints the results obtained using the logical operators
+//AND, OR, XOR for two numbers entered by the user from the keyboard.
+void Question7()
+{
+    int num1, num2;
+    Console.WriteLine("Enter the first number");
+    num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter the second number");
+    num2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("The result of the AND operator is {0}", num1 & num2);
+    Console.WriteLine("The result of the OR operator is {0}", num1 | num2);
+    Console.WriteLine("The result of the XOR operator is {0}", num1 ^ num2);
+}
+
+
+//Write a program that prints the sum of two numbers entered by the user from the keyboard
+//using the 3 bit right and 4 bit shift operators to the screen.
+void Question8()
+{
+    int num1, num2;
+    Console.WriteLine("Enter the first number");
+    num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter the second number");
+    num2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("The result of the 3 bit right shift operator is {0}", num1 >> 3);
+    Console.WriteLine("The result of the 4 bit left shift operator is {0}", num2 << 4);
+}
+
